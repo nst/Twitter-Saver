553f0fa66f429b7ad1b28bcdd8d5a06fba4d1e96
Nicolas Seriot <nicolas@seriot.ch>
2010-01-02 02:57:24 +0100

    handle authentication

--- twitter_saver.py
+++ twitter_saver.py
@@ -8,19 +8,21 @@ import sys
 import urllib2
 import json
 
-def user_readable_statuses_count(username):
+def install_authentication_handler(url, login, password):
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, url, login, password)
+    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+    opener = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
+
+def user_statuses_count(username):
     theurl = 'http://twitter.com/users/show.json?screen_name=%s' % username
     try:
         lines = urllib2.urlopen(theurl).readlines()
     except Exception, e:
-        print e
-    
-    d = json.loads(''.join(lines))
-    
-    if d['protected']:
         return 0
     
-    return int(d['statuses_count'])
+    return int(json.loads(''.join(lines))['statuses_count'])
 
 def user_timeline_statuses(username, page=1):
     theurl = 'http://twitter.com/statuses/user_timeline/%s.json?count=200&page=%d' % (username, page)
@@ -33,16 +35,26 @@ def user_timeline_statuses(username, page=1):
     return json.loads(''.join(lines))
 
 def print_statuses(username):
-    count = user_readable_statuses_count(username)
+    count = user_statuses_count(username)
     div, mod = divmod(count, 200)
     pages = div + (1 if mod else 0)
 
     for page in range(1, pages+1):
-        for d in user_timeline_statuses(username, page):
+        statuses = user_timeline_statuses(username, page)
+        if not statuses:
+            break
+        for d in statuses:
             print unicode("%(id)s\t%(created_at)s\t%(text)s" % d).encode('utf-8')
 
-if len(sys.argv) != 2:
-    print "USAGE: %s <username>" % sys.argv[0]
+if len(sys.argv) not in [2, 4]:
+    print "USAGE: %s <username> [<login> <password>]" % sys.argv[0]
     sys.exit(1)
 
-print_statuses(sys.argv[1])
+username = sys.argv[1]
+login = sys.argv[2] if len(sys.argv) == 4 else None
+password = sys.argv[3] if len(sys.argv) == 4 else None
+
+if login and password:
+    install_authentication_handler('http://twitter.com/statuses', login, password)
+
+print_statuses(username)
